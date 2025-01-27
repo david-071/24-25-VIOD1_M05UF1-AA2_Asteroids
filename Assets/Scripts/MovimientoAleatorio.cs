7d030e6a6e2792d13304f7d80cd4b3c2e7c8d641
Yuhao David Xia <yuhao.xia@enti.cat>
2025-01-27 13:59:31 +0100

    Teleporter y Comenzar movimiento

--- Assets/Scripts/MovimientoAleatorio.cs
+++ Assets/Scripts/MovimientoAleatorio.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class MovimientoAleatorio : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public float horizontal;
+    public float vertical;
+    public float speed;
     void Start()
     {
+        horizontal = Random.Range(-1f, 1f);
+        vertical = Random.Range(-1f, 1f);
+        speed = Random.Range(-1f, 1f);
+        rb = GetComponent<Rigidbody2D>();
+        //Transform.eulerAngles = transform.eulerAngles
 
     }
 }
